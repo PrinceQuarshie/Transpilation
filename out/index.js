ed02166f23e24329a7dd28af1de73829eeb09237
PrinceQuarshie <prince.quarshie@amalitech.org>
2022-08-12 14:17:01 +0000

    out|index.js

---
+++ out/index.js
@@ -0,0 +1,7 @@
+async function fetchUsers() {
+  const userIDs = await fetch("/users");
+  const users = await Promise.all(
+    userIDs.map((userID) => fetch(`/users/${userID}`))
+  );
+  return users;
+}
